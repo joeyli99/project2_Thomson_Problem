95bdd69aec2e85690125af1e3902462b843a0d2c
joeyli99 <44662994+joeyli99@users.noreply.github.com>
2019-05-09 17:13:05 +0800

    Update the main script
    Change the Boltzman constant, change the epoch number.
    
    Add some sample data in try_data.xlsx

--- code/anneal.m
+++ code/anneal.m
@@ -1,14 +1,15 @@
-N = 4;
-kb = 0.01;
+clc;
+clear;
+N = 30;
+kb = 0.001;
 map = rand_map(N);
 map_try = zeros(N, 2);
 energy_now = energy(map(:, 1), map(:, 2));
 for t = 500:-1:1
-  for epochs = 1:200
+  for epochs = 1:1000
     counter = 0;
     map_try(2:N, :) = map(2:N, :)+(rand(N-1, 2)-0.5)*pi/3;
     energy_try = energy(map_try(:, 1), map_try(:, 2));
-    counter = counter +1;
     if (energy_try < energy_now)
       map = map_try;
       energy_now = energy_try;
@@ -23,3 +24,4 @@ for t = 500:-1:1
   disp(energy_now)
 end
   map = spheical_norm(map);
+  disp(map)
